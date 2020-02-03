8a8d25a771713776a56261d1fe8385e7199ed718
Yuqing Lin <linyuqing97@gmail.com>
2020-02-03 10:41:15 -0500

    finished

---
+++ Project1/Main.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<list>
+
+
+using namespace std;
+unordered_map<int,string>stateRecord;
+vector<int>stageRecord;
+unordered_map<int,list<pair<int,int> > > transitionRecord;
+unordered_set<int>isVisited;
+
+bool compareId(const pair<int,int>&i,const pair<int,int>j){
+    return i .second<j.second;
+}
+void stateRecorder(string behavior, int id){
+    stateRecord[id]=behavior;
+}
+void transitionRecorder(int id,int approch, int result){
+    pair<int,int>p(approch,result);
+    transitionRecord[id].push_back(p);
+    transitionRecord[id].sort(compareId);
+}
+
+
+
+void NFSSimulator(string s,int startState){
+    //int curState = startState;
+    isVisited.insert(startState); // insert the startstate;
+    std::list<pair<int,int> >::iterator it;
+    stageRecord.push_back(startState);
+    list<pair<int,int> > temp;
+
+    //cout<<startState<<endl;
+
+    for (int i = 0; i < s.size(); i++){
+        int stageRecodsize = stageRecord.size();
+        for(int j = 0;j<stageRecodsize;j++){
+            temp = transitionRecord[stageRecord[j]];
+            bool isSelfRepeating = false;
+            for(std::list<pair<int,int> >::iterator it = temp.begin(); it != temp.end(); ++it){
+                //cout<<it->first<<" "<<it->second<<endl;
+                int c = s[i]-48;
+                if(it->first ==c && isVisited.find(it->second)==isVisited.end()){
+                    isVisited.insert(it->second);
+                    stageRecord.push_back(it->second);
+                }
+                if(it->first == c && stageRecord[j] == it->second){
+                    isSelfRepeating = true;
+                }
+             
+            }
+            if(!isSelfRepeating){
+                isVisited.erase(j);
+                stageRecord[j]=0;
+            }
+           
+
+        }
+    }
+    vector<int>acceptResult;
+    for(int i = 0;i<stageRecord.size();i++){
+        if(stateRecord[stageRecord[i]] == "accept"){
+            acceptResult.push_back(stageRecord[i]);
+        }
+    }
+
+    if(acceptResult.size() == 0){
+        cout<<"reject ";
+        for(int i = 0;i<stageRecord.size();i++){
+             if(stageRecord[i]!=0)
+            cout<<stageRecord[i]<<" ";
+        }
+        cout<<"\n";
+    }
+    else{
+        cout<<"accept ";
+        for(int i = 0;i<acceptResult.size();i++){
+            cout<<acceptResult[i]<<" ";
+        }
+        cout<<"\n";
+    }
+    
+}
+int main(int argc, char*argv[]){
+    //Check inputs
+    string filename = argv[1];
+    string input = argv[2];
+    ifstream infile(filename,std::ifstream::in);
+    string line;
+    string behavior;
+    int id;
+    int approch;
+    int result;
+    string name;
+    int startState;
+   
+    while(getline(infile,line)){
+        stringstream ss(line);
+        ss>>name;
+        if(name =="state"){
+            ss>>id>>behavior;
+            stateRecorder(behavior,id);
+            if(behavior=="start")
+            startState=id;
+        }
+        else if (name == "transition"){
+            ss>>id>>approch>>result;
+            transitionRecorder(id,approch,result);
+        }
+    }/*
+    cout<<stateRecord[7]<<endl;
+    cout<<stateRecord[1]<<endl;
+    for(std::list<pair<int,int> >::iterator it = transitionRecord[2].begin(); it != transitionRecord[2].end(); ++it)
+    cout<<it->first<<it->second<<endl;
+
+    through process：
+        after we have everything tracerable, we need to have a set to prevent duplicate after that, we need to main tain a queue to proces
+        the job with termination point of both input finished and queue is empty
+    */
+       NFSSimulator(input,startState);
+
+
+
+}
